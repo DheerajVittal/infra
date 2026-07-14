6ce2d73ea6c9799038a546615566839f1ed1ee18
DheeraVittal <dheerajbv@gmail.com>
2026-07-14 10:49:24 +0530

    config: update bucket name and github org for my account

---
+++ envs/prod/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket       = "zen-pharma-terraform-state-dheerajvittal"
+    key          = "envs/prod/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
